7b92de78f841dcb48f2d786be382ca792024e59e
jayjiahua <553544693@qq.com>
2016-05-24 16:31:05 +0800

    调用豆瓣电影api获取电影信息

--- src/main/java/com/c09/cinpockema/movie/entities/Movie.java
+++ src/main/java/com/c09/cinpockema/movie/entities/Movie.java
@@ -15,6 +15,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Range;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
@@ -26,11 +28,74 @@ public class Movie {
 
 	@NotNull
 	@Column(nullable=false)
-	private String name;
+	private String title;
+
+	private String originalTitle;
 
 	@NotNull
 	@Column(nullable=false)
-	private String description;
+	private double rating;
+
+	private String genres;
+
+	private String imageUrl;
+	
+	private String originalId;
+	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOriginalTitle() {
+		return originalTitle;
+	}
+
+	public void setOriginalTitle(String originalTitle) {
+		this.originalTitle = originalTitle;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public String getGenres() {
+		return genres;
+	}
+
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getDetailUrl() {
+		return "http://api.douban.com/v2/movie/subject/" + getOriginalId();
+	}
+
+
+	public String getOriginalId() {
+		return originalId;
+	}
+
+	public void setOriginalId(String originalId) {
+		this.originalId = originalId;
+	}
+
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "movie", fetch = FetchType.LAZY)    
 	List<MovieComment> movieComments = new ArrayList<MovieComment>();
@@ -43,21 +108,6 @@ public class Movie {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 
 	@JsonBackReference
 	public List<MovieComment> getMovieComments() {
--- src/main/java/com/c09/cinpockema/service/DataInitService.java
+++ src/main/java/com/c09/cinpockema/service/DataInitService.java
@@ -1,9 +1,14 @@
 package com.c09.cinpockema.service;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 
 import com.c09.cinpockema.movie.entities.Movie;
 import com.c09.cinpockema.movie.entities.MovieComment;
@@ -11,6 +16,7 @@ import com.c09.cinpockema.movie.entities.repositories.MovieCommentRepository;
 import com.c09.cinpockema.movie.entities.repositories.MovieRepository;
 import com.c09.cinpockema.user.entities.User;
 import com.c09.cinpockema.user.entities.repositories.UserRepository;
+import com.jayway.jsonpath.JsonPath;
 
 
 @Service
@@ -25,6 +31,8 @@ public class DataInitService {
     @Autowired
     MovieCommentRepository movieCommentRepository;
 
+    static private String DOUBAN_API = "http://api.douban.com/v2/movie/in_theaters?count=100";
+    
     @PostConstruct
     public void userDataInit(){
         User admin = new User();
@@ -46,27 +54,53 @@ public class DataInitService {
         user.setUsername("user-for-comment");
         user.setPassword("user");
         userRepository.save(user);
+        
+    	RestTemplate restTemplate = new RestTemplate();
+    	ResponseEntity<String> responseEntity = restTemplate.getForEntity(DOUBAN_API, String.class);
+    	String jsonResponse = responseEntity.getBody();
+    	System.err.println(jsonResponse);
+    	
+    	int total = JsonPath.read(jsonResponse, "$.total");
+    	List<Object> ratingList = JsonPath.read(jsonResponse, "$.subjects[*].rating.average");    	
+    	List<List<String>> genersList = JsonPath.read(jsonResponse, "$.subjects[*].genres");
+    	List<String> titleList = JsonPath.read(jsonResponse, "$.subjects[*].title");
+    	List<String> originalTitleList =  JsonPath.read(jsonResponse, "$.subjects[*].original_title");
+    	List<String> originalIdList = JsonPath.read(jsonResponse, "$.subjects[*].id");
+    	List<String> imageUrlList = JsonPath.read(jsonResponse, "$.subjects[*].images.medium");
 
-        for (int k = 0 ; k < 3 ; k++) {
+        for (int k = 0 ; k < total ; k++) {
+
 	        Movie movie = new Movie();
-	        movie.setName("movie-" + k);
-	        movie.setDescription("Some shit");
-	        movieRepository.save(movie);
-
-	        for (int i = 0 ; i < 3 ; i++) {
-		        MovieComment movieComment = new MovieComment();
-		        movieComment.setScore(i);
-		        movieComment.setContent("comment-" + i);
-
-		        // Warning: Don't do this or you will ...
-		        // user.addMovieComment(movieComment);
-		        // Is it a f**king bug ?
-		        // You should do as follows:
-		        movieComment.setUser(user);
-
-		        movie.addMovieComment(movieComment);
+	        
+	        Double rating;
+	        try {
+	        	rating = (Double) ratingList.get(k);
+	        } catch (Exception e) {
+	        	Integer temp = (Integer) ratingList.get(k);
+	        	rating = temp.doubleValue();
 	        }
+	        movie.setRating(rating);
+	        movie.setGenres(StringUtils.collectionToDelimitedString(genersList.get(k), ","));
+	        movie.setTitle(titleList.get(k));
+	        movie.setOriginalId(originalIdList.get(k));
+	        movie.setOriginalTitle(originalTitleList.get(k));
+	        movie.setImageUrl(imageUrlList.get(k));
 	        movieRepository.save(movie);
+//
+//	        for (int i = 0 ; i < 3 ; i++) {
+//		        MovieComment movieComment = new MovieComment();
+//		        movieComment.setScore(i);
+//		        movieComment.setContent("comment-" + i);
+//
+//		        // Warning: Don't do this or you will ...
+//		        // user.addMovieComment(movieComment);
+//		        // Is it a f**king bug ?
+//		        // You should do as follows:
+//		        movieComment.setUser(user);
+//
+//		        movie.addMovieComment(movieComment);
+//	        }
+//	        movieRepository.save(movie);
         }
     }
 
