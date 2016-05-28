ee27629e08f89d0d301fa5dedcd3ee903c4f5e73
fangshj <fangshj3@gmail.com>
2016-05-28 23:54:08 +0800

    增加了Ticket属性和对应方法，使得Screening和Hall形成一对一关系

--- src/main/java/com/c09/cinpockema/cinema/entities/Seat.java
+++ src/main/java/com/c09/cinpockema/cinema/entities/Seat.java
@@ -1,17 +1,25 @@
 package com.c09.cinpockema.cinema.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import com.c09.cinpockema.product.entities.Screening;
+import com.c09.cinpockema.product.entities.Ticket;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -71,4 +79,17 @@ public class Seat {
 		this.hall = hall;
 	}
 	
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "seat", fetch = FetchType.LAZY)    
+	@JoinColumn(name="ticket_id")
+	private Ticket ticket;
+	
+	@JsonBackReference
+	public Ticket getTicket() {
+		return ticket;
+	}
+
+	@JsonIgnore
+	public void setTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
 }
