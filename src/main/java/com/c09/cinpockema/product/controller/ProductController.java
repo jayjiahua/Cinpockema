c30282c1da0b6aa50609e5653f350544b9984a66
fangshj <fangshj3@gmail.com>
2016-05-31 13:53:48 +0800

    Product模块文档编写

--- src/main/java/com/c09/cinpockema/product/controller/ProductController.java
+++ src/main/java/com/c09/cinpockema/product/controller/ProductController.java
@@ -17,6 +17,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.c09.cinpockema.user.service.SessionService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import com.c09.cinpockema.product.service.ProductService;
 import com.c09.cinpockema.cinema.service.CinemaService;
 import com.c09.cinpockema.movie.service.MovieService;
@@ -29,6 +33,7 @@ import com.c09.cinpockema.cinema.entities.Seat;
 import com.c09.cinpockema.product.entities.Screening;
 import com.c09.cinpockema.product.entities.Ticket;
 
+@Api(value="产品模块", description="Screening、Ticket的CURD操作")
 @RestController
 @RequestMapping("/cinemas")
 public class ProductController {
@@ -36,16 +41,19 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@Autowired
-	private CinemaService cinemaService;
-	
-	// curl localhost:8080/api/cinemas/1/screenings/
+	@ApiOperation(value="根据影院ID获取所有场次")
 	@RequestMapping(value={"/{cinemaId}/screenings"}, method=RequestMethod.GET)
 	public List<Screening> listScreeningsByCinameId(@PathVariable("cinemaId") long cinemaId) {
 		return productService.listScreeningsByCinameId(cinemaId);
 	}
 	
-	// curl localhost:8080/api/cinemas/1/screenings -u admin:admin -H "Content-Type: application/json" -d "{\"tempHallId\":1, \"tempMovieId\":66666, \"startTime\":\"11:30\", \"runningTime\":120}"
+	@ApiOperation(value="根据场次ID获取一个场次")
+	@RequestMapping(value={"/{cinemaId}/screenings/{screeningId}"}, method=RequestMethod.GET)
+	public Screening getScreeningById(@PathVariable("screeningId") long screeningId) {
+		return productService.getScreeningById(screeningId);
+	}
+	
+	@ApiOperation(value="创建一个场次，场次的hallId、movieId封装在json内，key值为tempHallId、tempMovieId")
 	@RequestMapping(value = {"/{cinemaId}/screenings"}, method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     @PreAuthorize("hasAuthority('admin')")
@@ -53,7 +61,7 @@ public class ProductController {
 		return productService.createScreening(screening);
 	}
 	
-	// curl -X DELETE localhost:8080/api/cinemas/1/screenings/1 -u admin:admin
+	@ApiOperation(value="根据场次ID删除一个场次")
 	@RequestMapping(value={"/{cinemaId}/screenings/{screeningId}"}, method=RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @PreAuthorize("hasAuthority('admin')")
@@ -61,27 +69,29 @@ public class ProductController {
 		productService.deleteScreeningById(screeningId);
 	}
 	
-	// curl localhost:8080/api/cinemas/1/screenings/1/tickets/1
+	@ApiOperation(value="根据场次ID获取所有电影票")
+	@RequestMapping(value={"/{cinemaId}/screenings/{screeningId}/tickets"}, method=RequestMethod.GET)
+	public List<Ticket> listTicketsByScreeningId(@PathVariable("screeningId") long screeningId) {
+		return productService.listTicketsByScreeningId(screeningId);
+	}
+	
+	@ApiOperation(value="根据电影票ID获取电影票")
 	@RequestMapping(value={"/{cinemaId}/screenings/{screeningId}/tickets/{ticketId}"}, method=RequestMethod.GET)
 	public ResponseEntity<Ticket> getTicketById(@PathVariable("ticketId") long ticketId) {
 		Ticket ticket = productService.getTicketById(ticketId);
 		return new ResponseEntity<Ticket>(ticket, ticket != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
 	}
 	
-	// curl localhost:8080/api/product/tickets/1/1 -u admin:admin -H "Content-Type: application/json" -d "{"\"screeningId\":1, \"seatId\":1, \"ticket\":{\"price\":80}}"
+	@ApiOperation(value="创建一张电影票，电影票的seatId封装在json内，key值为tempSeatId")
 	@RequestMapping(value = {"/{cinemaId}/screenings/{screeningId}/tickets"}, method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     @PreAuthorize("hasAuthority('admin')")
-    public Ticket createTicket(
-    		@Valid @RequestParam(value = "ticket") Ticket ticket, 
-    		@RequestParam(value = "screeningId") long screeningId, 
-    		@RequestParam(value = "seatId") long seatId) {
+    public Ticket createTicket(@Valid @RequestBody Ticket ticket, @PathVariable("screeningId") long screeningId) {
 		Screening screening = productService.getScreeningById(screeningId);
-		Seat seat = cinemaService.getSeatById(seatId);
-		return productService.createTicket(ticket, screening, seat);
+		return productService.createTicket(ticket, screening);
 	}
 	
-	// curl -X DELETE localhost:8080/api/product/tickets/1 -u admin:admin
+	@ApiOperation(value="根据电影票ID删除一张电影票")
 	@RequestMapping(value={"/{cinemaId}/screenings/{screeningId}/tickets/{id}"}, method=RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @PreAuthorize("hasAuthority('admin')")
